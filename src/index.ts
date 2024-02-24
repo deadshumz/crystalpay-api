2ac2044975ca1527d05656fe70393e960b82e1f4
deadshumz <shumzz@bk.ru>
2024-02-24 13:02:58 +0300

    tests setup, configs

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,7 @@
-// TODO: Main api class
+class CrystalPay {
+    constructor(auth_login: string, auth_secret: string) {
+        console.log(auth_login, auth_secret)
+    }
+}
+
+export default CrystalPay;
